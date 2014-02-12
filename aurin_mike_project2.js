2692ef54768dc1cce41a723ae8e349559c42c6e1
Mike Aurin <mikeaurin@gmail.com>
2014-02-12 16:05:35 -0500

    Added a prompt requesting the user's name

--- aurin_mike_project2.js
+++ aurin_mike_project2.js
@@ -1,1 +1,41 @@
-alert("JavaScript works!");
+// Mike Aurin
+// SDI 1402 
+// Project 2
+/* My project this week will follow the same story but will encompass the newly learned
+   elements and follow the structure of the assigned flowchart.
+*/
+
+// Variables
+var myBoss = "Bernie",
+	theTheater = "Regal Cinemas at Sawgrass Mills".
+	numberOfBulbs = 4,
+	newEmployeePrompt;
+	
+newEmployeePrompt = prompt("You are my new employee!  What is your name?"); 
+console.log("Welcome to Theater Hands , " + newEmployeePrompt + "!");
+
+
+
+
+
+
+
+// Requirements:
+// Remember to comment code...and remove this.
+// Git commits!
+// One while loop and one for loop
+// At least two boolean logic expressions
+// One function that doesn't return a value (a procedure)
+// One function that takes two arguments and does a while loop with some kind of Boolean comparison between them. 
+//   One argument should be the value returned from the confirm and the other can be any data type you choose.
+// One function that does something interesting with strings after accepting two strings as arguments. 
+//   This can be simple concatenation, but you are encouraged to look into other, more interesting string applications.
+// One function that takes a single number and uses it in a for loop somehow, doing some math at the same time. 
+//   This can be something simple like counting or summing, but again you are encouraged to look into more interesting applications.
+   
+/*Since three of these functions return values, you should do something interesting with the values when you get them back. 
+  You are encouraged, but not required, to use the return values from earlier functions as the inputs for later functions. 
+  You'll need this for Project 3, so it's something you should get comfortable with now.
+*/
+
+// All functions take arguments
